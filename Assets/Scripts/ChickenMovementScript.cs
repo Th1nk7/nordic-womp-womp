5d6b93c1a14bb280dcf5a70a6037d83a6bd78414
Th1nk <Tobiasransborg@hotmail.com>
2024-04-20 14:25:20 +0200

    BasicallyWorkingGame
    Shit works now
    Movement
    Animations
    and stuff

---
+++ Assets/Scripts/ChickenMovementScript.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenMovementScript : MonoBehaviour
+{
+
+    public BoxCollider2D bc;
+    public bool MoveReady = true;
+    public Animator m_Animator;
+    public string direction;
+    public Transform transform;
+    public float TPDist = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveChicken(){
+        if(direction == "Down"){
+            transform.position = new Vector2(transform.position.x,transform.position.y-TPDist);
+        }
+
+        if(direction == "Right"){
+            transform.position = new Vector2(transform.position.x+TPDist,transform.position.y);
+        }
+
+        if(direction == "Up"){
+            transform.position = new Vector2(transform.position.x,transform.position.y+TPDist);
+        }
+
+        if(direction == "Left"){
+            transform.position = new Vector2(transform.position.x-TPDist,transform.position.y);
+        }
+    }
+
+    void AnimationDoneReceiver(){
+        MoveReady = true;
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(Input.GetKey(KeyCode.DownArrow) && col.gameObject.tag == "Down" && MoveReady == true){
+            Debug.Log("Down Correct!");
+            MoveReady = false;
+            direction = "Down";
+            m_Animator.SetTrigger("ChickenPoof");
+        }
+        if(Input.GetKey(KeyCode.UpArrow) && col.gameObject.tag == "Up" && MoveReady == true){
+            Debug.Log("Up Correct!");
+            MoveReady = false;
+            direction = "Up";
+            m_Animator.SetTrigger("ChickenPoof");
+        }
+        if(Input.GetKey(KeyCode.LeftArrow) && col.gameObject.tag == "Left" && MoveReady == true){
+            Debug.Log("Left Correct!");
+            MoveReady = false;
+            direction = "Left";
+            m_Animator.SetTrigger("ChickenPoof");
+        }
+        if(Input.GetKey(KeyCode.RightArrow) && col.gameObject.tag == "Right" && MoveReady == true){
+            Debug.Log("Right Correct!");
+            MoveReady = false;
+            direction = "Right";
+            m_Animator.SetTrigger("ChickenPoof");
+        }
+    }
+}
